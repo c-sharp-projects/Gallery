e927f5106e7f8fc87c7386f899c63d358be99afd
harshalghule1896 <harshalghule07@gmail.com>
2019-04-15 22:00:06 +0530

    Gallery added

---
+++ Gallery/ImageInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Gallery
+{
+    class ImageInfo
+    {
+        public BitmapImage ImageData { get; set; }
+        public string Title { get; set; }
+        public string Path { get; set; }
+
+    }
+
+    
+}
